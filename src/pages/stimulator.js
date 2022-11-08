bd1567d3e41aede02b735796ca56a069f762e7c3
avinashsoni9829 <avinashsoni9829@gmail.com>
2022-11-08 17:21:55 +0530

    Floors Display on Simulator Page

--- src/pages/stimulator.js
+++ src/pages/stimulator.js
@@ -1,7 +1,17 @@
-floor;
-lift;
-function logData(event){
-    event.preventDefault();
-    floor = parseInt(sessionStorage.getItem("floor"))
-    lift = parseInt(sessionStorage.getItem("lift"))
- }
+document.addEventListener("DOMContentLoaded",function(){
+         
+    let floor = sessionStorage.getItem("floor");
+      for (let index = 0; index < parseInt(floor); index++) {
+        const heading  = document.createElement("p");
+        const node = document.createTextNode("floor " + (index + 1).toString());
+        heading.appendChild(node);
+
+        const division = document.getElementById("div1");
+        division.append(heading);
+            
+         }
+         
+   
+     
+})
+ 
