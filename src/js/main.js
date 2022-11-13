3a4e159b1d7265c63c53759f7cf310e4b6eddc6b
avinashsoni9829 <avinashsoni9829@gmail.com>
2022-11-13 20:32:39 +0530

    added buttons and floor completed

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,8 @@ function submitDetails(event) {
     console.log(no_of_lifts.value);
     if(parseInt(no_of_floors.value) > parseInt(no_of_lifts.value)){
         document.getElementById("errorMessage").style.visibility  = "hidden";
+
+        window.location.href="http://127.0.0.1:5500/src/pages/stimulator.html";
     }
 
     if(parseInt(no_of_floors.value) < parseInt(no_of_lifts.value)){
--- src/pages/stimulator.js
+++ src/pages/stimulator.js
@@ -1,17 +1,37 @@
 document.addEventListener("DOMContentLoaded",function(){
          
     let floor = sessionStorage.getItem("floor");
+      
       for (let index = 0; index < parseInt(floor); index++) {
         const heading  = document.createElement("p");
-        const node = document.createTextNode("floor " + (index + 1).toString());
+        heading.className = "floor"
+        const node = document.createTextNode("floor " + (floor - index).toString());
         heading.appendChild(node);
+        const div = document.createElement("div");
+        div.className ="line";
 
+        const up_button = document.createElement("button");
+        up_button.innerHTML="up";
+        up_button.className ="btn";
+        const down_button = document.createElement("button");
+        down_button.innerHTML="down";
+        down_button.className ="btn";
         const division = document.getElementById("div1");
         division.append(heading);
+        if(index == floor - 1){
+          division.append(up_button)
+       }
+       else
+       {
+          division.append(up_button);
+          division.append(down_button);
+       }
+        division.append(div);
+       
             
      }
-         
-   
-     
+      
 })
+
+
  
